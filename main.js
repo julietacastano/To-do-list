d1de36899d1c0ea61277b34d48593059100ec6d8
julieta.castano <julieta.castanosanchez@gmail.com>
2022-07-19 10:34:11 -0300

    Desafío: Incorporar Arrays

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
 let tareasCompletas = 0;
 let tareasIcompletas = 0;
+alert('Tenes 4 tareas pendientes')
 
 function Tareas (tarea, tablero, estado, prioridad){
     this.tarea = tarea;
@@ -12,11 +13,9 @@ const tarea1 = new Tareas ('Tarea 1', 'Personal', prompt('Tu tarea esta completa
 const tarea2 = new Tareas ('Tarea 2', 'Trabajo', prompt('Tu tarea esta completa o incompleta?').toLowerCase(), 3);
 const tarea3 = new Tareas ('Tarea 3', 'Personal', prompt('Tu tarea esta completa o incompleta?').toLowerCase(), 4);
 const tarea4 = new Tareas ('Tarea 4', 'Hogar', prompt('Tu tarea esta completa o incompleta?').toLowerCase(), 6);
-const tarea5 = new Tareas ('Tarea 5', 'Facultad', prompt('Tu tarea esta completa o incompleta?').toLowerCase(), 2);
-const tarea6 = new Tareas ('Tarea 6', 'Personal', prompt('Tu tarea esta completa o incompleta?').toLowerCase(), 5);
-console.log(tarea1, tarea2, tarea3, tarea4, tarea5, tarea6);
+console.log(tarea1, tarea2, tarea3, tarea4);
 
-const listadoDeEstados = [tarea1.estado,tarea2.estado,tarea3.estado,tarea4.estado,tarea5.estado,tarea6.estado];
+const listadoDeEstados = [tarea1.estado,tarea2.estado,tarea3.estado,tarea4.estado];
 
 for(let i=0; i<=listadoDeEstados.length; i++){
     if(listadoDeEstados[i]=='completa'){
@@ -36,14 +35,8 @@ if(tareasCompletas==listadoDeEstados.length){
     console.log('Vamos! Ya completaste la mayoria de tus tareas');
 }else{
     console.log('A seguir completando tareas.');
-}
+} 
 
-let suma=0;
-function TareasSemanales(semana1, semana2, semana3, semana4) {
-    suma= semana1+semana2+semana3+semana4;
-}
-
-TareasSemanales (4,8,6,1);
-console.log("Tareas realizadas en el mes:" + suma);
-
+const suma = (semana1, semana2, semana3, semana4) => semana1+semana2+semana3+semana4;
+console.log("Tareas realizadas en el mes:" + suma (4,8,6,1));
 
