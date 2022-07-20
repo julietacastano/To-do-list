aafa91e46cdaea10b2bf28de0847a3f1eb3c7a25
julieta.castano <julieta.castanosanchez@gmail.com>
2022-07-20 11:30:32 -0300

    Primera entrega del Proyecto Final

--- main.js
+++ main.js
@@ -1,42 +1,47 @@
+const listaDeTareas = [];
 let tareasCompletas = 0;
 let tareasIcompletas = 0;
-alert('Tenes 4 tareas pendientes')
+alert("Tenes 4 tareas pendientes");
 
-function Tareas (tarea, tablero, estado, prioridad){
-    this.tarea = tarea;
-    this.tablero = tablero;
-    this.estado = estado;
-    this.prioridad = prioridad;
+function AgregarTarea (tarea){
+    listaDeTareas.push (tarea);
 }
 
-const tarea1 = new Tareas ('Tarea 1', 'Personal', prompt('Tu tarea esta completa o incompleta?').toLowerCase(), 1);
-const tarea2 = new Tareas ('Tarea 2', 'Trabajo', prompt('Tu tarea esta completa o incompleta?').toLowerCase(), 3);
-const tarea3 = new Tareas ('Tarea 3', 'Personal', prompt('Tu tarea esta completa o incompleta?').toLowerCase(), 4);
-const tarea4 = new Tareas ('Tarea 4', 'Hogar', prompt('Tu tarea esta completa o incompleta?').toLowerCase(), 6);
-console.log(tarea1, tarea2, tarea3, tarea4);
+AgregarTarea({id:1, tarea:"Tarea 1", tablero:"Personal", estado:prompt('Tu tarea esta completa o incompleta?').toLowerCase(), importancia: "Alta"});
+AgregarTarea({id:2, tarea:"Tarea 2", tablero:"Personal", estado:prompt('Tu tarea esta completa o incompleta?').toLowerCase(), importancia: "Baja"});
+AgregarTarea({id:3, tarea:"Tarea 3", tablero:"Trabajo", estado:prompt('Tu tarea esta completa o incompleta?').toLowerCase(), importancia: "Normal"});
+AgregarTarea({id:4, tarea:"Tarea 4", tablero:"Casa", estado:prompt('Tu tarea esta completa o incompleta?').toLowerCase(), importancia: "Normal"});
 
-const listadoDeEstados = [tarea1.estado,tarea2.estado,tarea3.estado,tarea4.estado];
+console.log(listaDeTareas);
 
-for(let i=0; i<=listadoDeEstados.length; i++){
-    if(listadoDeEstados[i]=='completa'){
+const listaDeEstados = listaDeTareas.map ((el)=> el.estado);
+console.log (listaDeEstados);
+
+for(let i=0; i<=listaDeEstados.length; i++){
+    if(listaDeEstados[i]=='completa'){
         tareasCompletas++;
-    }else if(listadoDeEstados[i]=='incompleta'){
+    }else if(listaDeEstados[i]=='incompleta'){
         tareasIcompletas++;
     }
 }
+
 console.log('Cantidad de tareas completas: '+ tareasCompletas);
 console.log('Cantidad de tareas incompletas: '+ tareasIcompletas);
 
-if(tareasCompletas==listadoDeEstados.length){
+if(tareasCompletas==listaDeEstados.length){
     console.log('Felicitaciones! Ya completaste todas las tareas');
-}else if(tareasCompletas==listadoDeEstados.length/2){
+}else if(tareasCompletas==listaDeEstados.length/2){
     console.log('Ya estas por la mitad de tu lista, segui asi!');
-}else if(tareasCompletas>listadoDeEstados.length/2){
+}else if(tareasCompletas>listaDeEstados.length/2){
     console.log('Vamos! Ya completaste la mayoria de tus tareas');
 }else{
     console.log('A seguir completando tareas.');
 } 
 
-const suma = (semana1, semana2, semana3, semana4) => semana1+semana2+semana3+semana4;
-console.log("Tareas realizadas en el mes:" + suma (4,8,6,1));
+function BorrarTarea (tareaParaBorrar) {
+    const index = listaDeTareas.findIndex((el)=> el.id === tareaParaBorrar);
+    listaDeTareas.splice(index,1);
+    console.log(listaDeTareas);
+}
 
+BorrarTarea(3);
